191b40b259a78e95653179fd587b55f3d752e3a5
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-08-31 14:16:37 +0530

    Runner Interfaces

---
+++ 03-Runners-App/src/main/java/com/ashu/runners/AppRunner.java
@@ -0,0 +1,16 @@
+package com.ashu.runners;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Application runner is called...");
+		
+	}
+
+}
