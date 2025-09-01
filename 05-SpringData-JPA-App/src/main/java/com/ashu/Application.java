6f53aa663ca668359c99f9c2877f62b421fd0bc7
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-09-01 13:35:33 +0530

    save(e) & findAll()

--- 05-SpringData-JPA-App/src/main/java/com/ashu/Application.java
+++ 05-SpringData-JPA-App/src/main/java/com/ashu/Application.java
@@ -15,14 +15,19 @@ public class Application {
 		
 		StudentRepository repo = context.getBean(StudentRepository.class);
 		
-		Student s = new Student();
-		s.setId(101);
-		s.setName("Ashu");
-		s.setRank(4050l);
-		s.setGender("Male");
+//		Student s = new Student();
+//		s.setId(103);
+//		s.setName("Ashmita");
+//		s.setRank(4052l);
+//		s.setGender("Female");
+//		
+//		repo.save(s);
+//		System.out.println("Record is inserted....");
 		
-		repo.save(s);
-		System.out.println("Record is inserted....");
+		
+		Iterable<Student> findAll = repo.findAll();
+		
+		findAll.forEach(s -> System.out.println(s));
 	}
 
 }
