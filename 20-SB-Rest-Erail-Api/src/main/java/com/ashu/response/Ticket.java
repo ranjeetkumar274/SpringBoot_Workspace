88b4fe6acfb9b6feb93a9347b4a72b49f68fc240
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-09-08 17:15:28 +0530

    bookTicket() & getTicket()

---
+++ 20-SB-Rest-Erail-Api/src/main/java/com/ashu/response/Ticket.java
@@ -0,0 +1,51 @@
+package com.ashu.response;
+
+public class Ticket {
+	
+	private long pnr;
+	private String status;
+	private String from;
+	private String to;
+	private String doj;
+	private String trainNo;
+	
+	
+	public long getPnr() {
+		return pnr;
+	}
+	public void setPnr(long pnr) {
+		this.pnr = pnr;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	public String getTrainNo() {
+		return trainNo;
+	}
+	public void setTrainNo(String trainNo) {
+		this.trainNo = trainNo;
+	}
+	
+	
+}
---
+++ 20-SB-Rest-Erail-Api/src/main/java/com/ashu/rest/ErailRestController.java
@@ -0,0 +1,59 @@
+package com.ashu.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ashu.request.Passenger;
+import com.ashu.response.Ticket;
+
+@RestController
+public class ErailRestController {
+	
+	private Map<Long, Ticket> ticketDb = new HashMap<>();
+	
+	
+	@PostMapping(
+			value = "/bookticket",
+			consumes = "application/json",
+			produces = "application/json"
+			)
+	public Ticket bookTicket(@RequestBody Passenger passenger) {
+		
+		Random ran = new Random();
+		long pnr = ran.nextLong(1000000000L, 9999999999L);
+		
+		Ticket t = new Ticket();
+		t.setPnr(pnr);
+		t.setStatus("CONFIRMED");
+		t.setFrom(passenger.getFrom());
+		t.setTo(passenger.getTo());
+		t.setDoj(passenger.getDoj());
+		t.setTrainNo(passenger.getTrainNo());
+		
+		ticketDb.put(pnr, t);
+		System.out.println("Ticket booked with pnr:POST: "+pnr);
+		
+		return t;
+	}
+	
+	
+	@GetMapping(
+			value = "/ticket/{pnr}",
+			produces = "application/json"
+			)
+	public Ticket getTicket(@PathVariable("pnr") long pnr) {
+		if(ticketDb.containsKey(pnr)) {
+			System.out.println("Ticket found for pnr :GET: "+pnr);
+			return ticketDb.get(pnr);
+			
+		}
+		return null;
+	}
+}
