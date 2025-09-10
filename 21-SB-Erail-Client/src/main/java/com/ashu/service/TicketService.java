54199c164ef69615add113416b7cfc2c0da8ade3
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-09-10 20:07:59 +0530

    removal of hardcoded apis

--- 21-SB-Erail-Client/src/main/java/com/ashu/service/TicketService.java
+++ 21-SB-Erail-Client/src/main/java/com/ashu/service/TicketService.java
@@ -1,5 +1,6 @@
 package com.ashu.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,11 +11,15 @@ import com.ashu.response.Ticket;
 @Service
 public class TicketService {
 	
-	private static final String ERAIL_API_KEY = "http://54.167.78.42:8080/bookticket";
+	@Value("${erail.api.key.booking}")
+	private String ERAIL_API_1;
+	
+	@Value("${erail.api.key.search}")
+	private String ERAIL_API_2;
 	
 	public Ticket ticketBooking(Passenger passenger) {
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Ticket> response = restTemplate.postForEntity(ERAIL_API_KEY, passenger, Ticket.class);
+		ResponseEntity<Ticket> response = restTemplate.postForEntity(ERAIL_API_1, passenger, Ticket.class);
 		int statusCode = response.getStatusCodeValue();
 		if(statusCode == 200) {
 			return response.getBody();
