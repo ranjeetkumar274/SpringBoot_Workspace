65f4e989d24d20c99048f8fe270f3c5d2ac9adeb
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-09-18 19:27:45 +0530

    settled with config-server and client for it

---
+++ 31-Config-Server-Client/src/main/java/com/ashu/rest/GreetRest.java
@@ -0,0 +1,18 @@
+package com.ashu.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController	
+public class GreetRest {
+	
+	@Value("${msg}")
+	private String msg;
+
+	@GetMapping("/")
+	public String getWelcomeMsg() {
+		return msg;
+	}
+
+}
