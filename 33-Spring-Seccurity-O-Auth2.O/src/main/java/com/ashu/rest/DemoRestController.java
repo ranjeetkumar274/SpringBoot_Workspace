6f0f865759a74a54b9d07cc3fac4ec26a6c54d1c
RANJEET KUMAR <2023073045@mmmut.ac.in>
2025-09-19 18:14:02 +0530

    OAuth 2.O implemented with github

---
+++ 33-Spring-Seccurity-O-Auth2.O/src/main/java/com/ashu/rest/DemoRestController.java
@@ -0,0 +1,13 @@
+package com.ashu.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+}
